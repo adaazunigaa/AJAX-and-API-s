eabd8a034fbf2434f766643a0601692fa792ebbc
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-03-01 23:05:49 -0600

    AXIOS
    We can
    -specify headers
    -manipulating the DOM

--- AXIOS/axios.js
+++ AXIOS/axios.js
@@ -18,6 +18,39 @@ async function getStarWarsPerson(id) {
 
 }
 
-for (let i = 1 ; i <= 3; i++){
+for (let i = 1; i <= 2; i++) {
     getStarWarsPerson(i);
 }
+
+
+
+const jokes = document.querySelector("#jokes");
+
+const btn = document.querySelector("#btn");
+
+
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: "application/json" } }
+        const res = await axios.get("https://icanhazdadjoke.com/", config)
+        //console.log(res);
+        //console.log(res.data.joke);
+        return res.data.joke;
+    } catch (e) {
+        return "No jokes available! SORRY :("
+    }
+
+}
+
+
+async function addNewJoke() {
+    const getJokeText = await getDadJoke();
+    const newLI = document.createElement("LI");
+    //newLI.innerText(res.data.joke)
+    //newLI.append(res.data.joke);
+    newLI.append(getJokeText);
+    jokes.append(newLI);
+}
+
+
+btn.addEventListener("click", addNewJoke);
