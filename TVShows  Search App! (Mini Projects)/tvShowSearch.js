568f4f1e4602eee63ac6fb18214827c6c1d36ad9
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-03-16 18:27:32 -0500

    style edited

--- TVShows  Search App! (Mini Projects)/tvShowSearch.js
+++ TVShows  Search App! (Mini Projects)/tvShowSearch.js
@@ -6,24 +6,36 @@ form.addEventListener("submit", async function (e) {
 
     const config = { params: { q: userInputSearch } }
     const res = await axios.get(`https://api.tvmaze.com/search/shows?`, config);
-    console.log(res.data[0].show.image.medium);
-    makeImages(res.data);
+    //console.log(res.data[0].show.image.medium);
+    getData(res.data);
     form.elements.query.value = "";
 });
 
-function makeImages(shows) {
+//res.data[0].show.name
+//res.data[0].show.summary
+function getData(showsJSON) {
 
-    for (let eachShow of shows) {
+    for (let eachShow of showsJSON) {
         if (eachShow.show.image) {
             const img = document.createElement("IMG");
+            //const name = document.createElement("figcaption");
+
+            
+            
             img.src = eachShow.show.image.medium;
+            //name.innerHTML = eachShow.show.name;
+
+
             img.className = "m-2 dottedBorder images"
-            document.querySelector(".displayImg").appendChild(img);
+            //name.className = "d-inline"
+            
+            //document.querySelector(".displayImg").append(name);
+            document.querySelector(".displayImg").append(img);
+           
+
         }
     }
 }
-
-
 
 
 
